43f2144c0f722f1263546fe93eae49fcb2ae6ca6
Alan Flood <alanwflood@gmail.com>
2017-11-25 23:30:45 +0000

    Searchbar work

---
+++ src/components/searchbar/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class searchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" name="search" />
+        <button type="submit">An arrow needs to go here</button>
+      </div>
+    );
+  }
+}
