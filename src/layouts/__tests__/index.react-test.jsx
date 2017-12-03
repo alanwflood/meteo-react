38be612bfe3d8193c305314a55136e76a382de3f
Alan Flood <alanwflood@gmail.com>
2017-12-03 20:29:57 +0000

    More render testing

--- src/layouts/__tests__/index.react-test.jsx
+++ src/layouts/__tests__/index.react-test.jsx
@@ -1,1 +1,10 @@
 import React from "react";
+import renderer from "react-test-renderer";
+import App from "../index";
+
+test("Search Bar renders and Logo Shows", () => {
+  const component = renderer.create(<App />);
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
